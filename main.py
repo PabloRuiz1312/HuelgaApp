f8aea59b722f066336828f266a8c9cb7240fd7cc
PabloRuiz1312 <pruican1312@g.educaand.es>
2023-10-27 11:50:01 +0200

    Prueba con botones

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import toga
-
+def onPress(widget):
+    print("Hola mundo")
+def new_button():
+    button = toga.Button(text="Boton prueba", on_press=onPress)
+    return button
 def build(app):
     box = toga.Box()
+    boton = new_button()
+    box.add(boton)
     return box
 def main():
-    app = toga.App('HuelgaApp','es.iesjandula.toga',startup=build,author="Pablo Ruiz Canovas",description="App que gestiona los alumnos que van a la huelga",home_page="https://www.iesjandula.es/drupal/")
+    app = toga.App('HuelgaApp','es.iesjandula.toga',startup=build,author="Pablo Ruiz Canovas",description="App que gestiona los alumnos que van a la huelga",home_page="https://www.iesjandula.es/drupal/",)
     return app
 
 if __name__ == "__main__":
