7cb09799cc3db9a3068694ff5493a72f61ddf01d
PabloRuiz1312 <pruican1312@g.educaand.es>
2023-11-07 12:45:45 +0100

    Se ajusta la clase al constructor

--- connection/mongo_connection.py
+++ connection/mongo_connection.py
@@ -1,18 +1,29 @@
 from pymongo import MongoClient
+from pymongo.database import Database
+from pymongo.collection import Collection
 from exception.mongo_exception import MongoException
 from logging import fatal
 URL = "mongodb+srv://admin:iesjandula@huelgadb.lboe9th.mongodb.net/?retryWrites=true&w=majority"
 class MongoConnection:
-    def connection(databaseName:str,collectionName:str):
-        client = None
-        database = None
-        collection = None
+    def __init__(self,databaseName:str,collectionName:str) -> None:
+        """Constructor que crea los atributos de la conexion"""
+        self.databaseName = databaseName
+        self.collectionName = collectionName
+        self.client = None
+        self.database = None
+        self.collection = None
+    def connection(self) -> MongoClient | Database | Collection:
+        """Metodo que conecta con la base de datos de mongodb Atlas\n
+        Returns:\n 
+        Cliente que contacta con atlas\n
+        Base de datos con la que se trabaja\n
+        Coleccion con la que se trabaja"""
         try:
-            client = MongoClient(URL)
-            database = client[databaseName]
-            collection = database[collectionName]
+            self.client = MongoClient(URL)
+            self.database = self.client[self.databaseName]
+            self.collection = self.database[self.collectionName]
         except MongoException:
             fatal("Error al conectar con la base de datos")
             
-        return client,database,collection
+        return self.client,self.database,self.collection
         
