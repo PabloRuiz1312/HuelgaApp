8f8cadf30a456611055466983199071969d4e352
PabloRuiz1312 <pruican1312@g.educaand.es>
2023-11-08 09:23:39 +0100

    Cambios en la ventana principal, se consigue centrar los botones

--- widgets/home_window.py
+++ widgets/home_window.py
@@ -1,6 +1,8 @@
 from toga import App
 from toga import Box
 from toga import Button
+from toga.style.pack import *
+from toga import MainWindow
 class HomeWindow:
     """
     Clase que se encarga de controlar la ventana principal al iniciar la app y de llamar \n
@@ -10,7 +12,13 @@ class HomeWindow:
         '''
         Constructor que crea la ventana principal
         '''
-        self.app = App('HuelgaApp','es.iesjandula.toga',startup=self.build,author="Pablo Ruiz Canovas",description="App que gestiona los alumnos que van a la huelga",home_page="https://www.iesjandula.es/drupal/",)
+        self.app = App(formal_name='HuelgaApp',
+                       app_id='es.iesjandula.toga',
+                       startup=self.build,
+                       author="Pablo Ruiz Canovas",
+                       description="App que gestiona los alumnos que van a la huelga",
+                       home_page="https://www.iesjandula.es/drupal/"
+                       )#App
         self.vent1 = vent1
         self.vent2 = vent2
         self.vent3 = vent3
@@ -19,20 +27,27 @@ class HomeWindow:
         """
         Contenedor de la ventana principal que guarda botones
         """
-        box = Box()
+        self.mainWindow = MainWindow(id="es.iesjandula.toga",title="HuelgaApp",size=(250,480),resizeable=False)
+        box = Box(style=Pack(direction=COLUMN))
         botonCrearHuelga = self.crearHuelga()
         botonConsultarHuelga = self.consultarHuelgas()
+        botonConsultaHuelgaCurso = self.crearHuelgaCurso()
+        botonSalir = self.crearBotonSalir()
         box.add(botonCrearHuelga)
         box.add(botonConsultarHuelga)
-        return box
+        box.add(botonConsultaHuelgaCurso)
+        box.add(botonSalir)
+        self.mainWindow.content = box
+        self.mainWindow.show()
+        return self.mainWindow
 
     def crearHuelga(self):
         """
         Boton para crear una huelga\n
-        Retorna el boton
+        Returns: El boton con sus atributos
         """
         button = Button(text="Crear huelga", on_press=self.onPressCrearHuelga)#, on_press=onPress)
-        button.style.padding = 45
+        button.style.padding = 30
         button.style.font_size = 14
         button.style.width = 200
         return button
@@ -40,11 +55,32 @@ class HomeWindow:
     def consultarHuelgas(self):
         """
         Boton para consultar una huelga \n
-        Retorna el boton
+        Returns: El boton con sus atributos
         """
         button = Button(text="Consultar huelga")#, on_press=onPress)
-        button.style.padding = 45
-        
+        button.style.padding = 30
+        button.style.font_size = 14
+        button.style.width = 200
+        return button
+    
+    def crearHuelgaCurso(self):
+        """
+        Boton para consultar una huelga por cursos\n
+        Returns: El boton con sus atributos
+        """
+        button = Button(text="Consultar huelga curso")
+        button.style.padding = 30
+        button.style.font_size = 14
+        button.style.width = 200
+        return button
+
+    def crearBotonSalir(self):
+        """
+        Boton para salir de la aplicacion\n
+        Returns: El boton con sus atributos
+        """
+        button = Button(text="Salir",on_press=self.onPressSalir)
+        button.style.padding = 30
         button.style.font_size = 14
         button.style.width = 200
         return button
@@ -53,5 +89,12 @@ class HomeWindow:
         self.vent1=False
         self.vent2=True
         self.app.exit()
+
+    def onPressSalir(self,widget):
+        self.vent1 = False
+        self.vent2 = False
+        self.vent3 = False
+        self.app.exit()
+
     def listener(self):
         return self.vent1,self.vent2,self.vent3
