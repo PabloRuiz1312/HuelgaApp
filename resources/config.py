4a7c8f81ddefcb80a09db50476a28fda70ad0e50
PabloRuiz1312 <pruican1312@g.educaand.es>
2023-11-10 10:58:14 +0100

    Se añade el filtrado por curso

--- resources/config.py
+++ resources/config.py
@@ -1,4 +1,5 @@
 from connection.students_file import Alumno
+from logging import warning
 class ConfigAlumnos:
     """
     Clase que se encarga de guardar y gestionar los alumnos recibidos por el fichero
@@ -11,5 +12,20 @@ class ConfigAlumnos:
         """
         self.alumnos = alumnos
     
-    def organizarPorCurso(self) -> list[list[Alumno]]:
-        alumnosCurso:list[list[Alumno]] = []
+    def organizarPorCurso(self,curso:str) -> list[Alumno]:
+        """
+        Metodo que organiza los alumnos por el curso introducido como parameto\n
+        PARAMS:\n
+        curso: Curso para filtrar los alumnos por el mismo\n
+        RETURN:\n
+        Lista de alumnos filtrados por el curso
+        """
+        alumnosCurso:list[Alumno] = []
+        for alumno in self.alumnos:
+            if(alumno.curso==curso):
+                alumnosCurso.append(alumno)
+        if(alumnosCurso.__len__()==0):
+            warning("No hay alumnos con el curso "+curso)
+        return alumnosCurso
+    
+
