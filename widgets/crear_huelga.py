d0ba3b2f871a34ea8c4f470eb673d2f4d1b5eb9c
PabloRuiz1312 <pruican1312@g.educaand.es>
2023-11-14 00:14:56 +0100

    Se añaden widgets al apartado de crear huelgas

--- widgets/crear_huelga.py
+++ widgets/crear_huelga.py
@@ -5,7 +5,10 @@ from toga import Label
 from toga import TextInput
 from toga.style.pack import *
 from toga import MainWindow
-
+from toga import Selection
+from connection.students_file import StudentsFile
+from connection.students_file import Alumno
+ALUMNOS_OPERATOR = StudentsFile()
 class CrearHuelga (App):
     """
     Clase que se encarga de gestionar la ventana donde se crea una huelga
@@ -34,49 +37,103 @@ class CrearHuelga (App):
         mainBox = Box(style=Pack(direction=COLUMN))
         #PRIMERA COLUMNA
         boxLabelsCurso = Box(style=Pack(direction=ROW))
-        boxLabelsCurso.add(self.createSeparator(cantidad=100))
+        boxLabelsCurso.add(self.createSeparator(cantidad=100,id="separatorCurso"))
         boxLabelsCurso.add(self.createLabelCurso())
-        boxLabelsCurso.add(self.createInputCurso())
+        self.selectionCurso = self.createSelectionCurso()
+        boxLabelsCurso.add(self.selectionCurso)
+        #self.inputCurso = self.createInputCurso()
+        #boxLabelsCurso.add(self.inputCurso)
         #SEGUNDA COLUMNA
         boxLabelsHuelga = Box(style=Pack(direction=ROW))
+        boxLabelsHuelga.add(self.createSeparator(cantidad=130,id="separatorHuelga"))
+        boxLabelsHuelga.add(self.createLabelHuelga())
+        self.inputHuelga = self.createInputHuelga()
+        boxLabelsHuelga.add(self.inputHuelga)
+        #TERCERA COLUMNA
+        boxAlumnos = Box(style=Pack(direction=ROW))
+        boxAlumnos.add(self.createSeparator(cantidad=100,id="separatorAlumnos"))
+        boxAlumnos.add(self.createLabelAlumnos())
+        self.selectionAlumnos = self.createSelectionAlumno()
+        boxAlumnos.add(self.selectionAlumnos)
+        #BOX PRINCIPAL
         mainBox.add(boxLabelsCurso)
         mainBox.add(boxLabelsHuelga)
+        mainBox.add(boxAlumnos)
         self.main_window.content = mainBox
-
         self.main_window.show()
-    
     def createLabelCurso(self):
         """
         Metodo que crea un label con curso como identificador
         """
-        curso = Label(text="Curso",id="LabelCurso")
+        curso = Label(text="Curso:",id="LabelCurso")
         curso.style.padding = 30
         curso.style.font_size = 14
         curso.style.width = 60
         return curso
     
+    def createLabelHuelga(self):
+        """
+        Metodo que crea un label con huelga como identificador
+        """
+        huelga = Label(text="Huelga:",id="LabelHuelga")
+        huelga.style.font_size = 14
+        huelga.style.width = 122
+        return huelga
+    
+    def createLabelAlumnos(self):
+        """
+        Metodo que crea un label con alumno como identificador
+        """
+        alumno = Label(text="Alumnos:",id="LabelAlumnos")
+        alumno.style.font_size = 14
+        alumno.style.width = 90
+        alumno.style.padding = 30
+        return alumno
+    
     def createInputCurso(self):
         """
-        Metodo que crea una caja de texto para introducir datos
+        Metodo que crea una caja de texto para introducir datos\n
+        !DEPRECATED Actualmente esta en desarrollo otro metodo que elige los cursos 
         """
         inputCurso = TextInput(id="InputCurso",placeholder="Introduce el curso")
         inputCurso.style.padding = 32
         inputCurso.style.width = 200
         return inputCurso
+
+    def createSelectionCurso(self):
+        """
+        Metodo que crea una caja donde puedes seleccionar los cursos que existen
+        """
+        listaCursos = self.obtenerCursos()
+        selectionCurso = Selection(id="SelectionCurso",enabled=True,items=listaCursos,on_select=self.onChangeCurso)
+        selectionCurso.style.padding = 32
+        selectionCurso.style.width = 200
+        return selectionCurso
     
-    def createLabelHuelga(self):
+    def createInputHuelga(self):
         """
-        Metodo que crea un label con huelga como identificador
+        Metodo que crea una caja de texto para introducir datos
         """
-        huelga = Label(text="Huelga",id="LabelHuelga")
-        huelga
-    def createSeparator(self,cantidad):
+        inputHuelga = TextInput(id="InputHuelga",placeholder="--/--/----")
+        inputHuelga.style.width = 200
+        return inputHuelga 
+
+    def createSelectionAlumno(self):
+        """
+        Metodo que que crea una caja donde puedes seleccionar los alumnos que existen
+        """ 
+        selectionAlumnos = Selection(id="SelectionAlumnos")
+        selectionAlumnos.style.width = 200
+        selectionAlumnos.style.padding = 30
+        return selectionAlumnos
+    
+    def createSeparator(self,cantidad:int,id:str):
         """
         Metodo que separa horizontalmente widgets\n
         PARAMETROS:\n
         canridad: Cantidad de pixeles a separar
         """
-        separator = Label(text="",id="Separator")
+        separator = Label(text="",id=id)
         separator.style.width = cantidad
         return separator
     
@@ -89,3 +146,47 @@ class CrearHuelga (App):
         self.vent1 = False
         self.vent2 = False
         self.vent3 = False
+
+    def obtenerCursos(self) -> list[str]:
+        """
+        Metodo que recoge los cursos del fichero de alumnos para guardarlos en un selection
+        """
+        self.cursos = []
+        alumnos:list[Alumno] = ALUMNOS_OPERATOR.readFile()
+        for curso in alumnos:
+            if(self.cursos.__len__()==0): 
+                self.cursos.append(curso.curso)
+            elif(self.comprobarCursoRepetido(cursos=self.cursos,checkCurso=curso.curso)):
+                self.cursos.append(curso.curso)
+        self.cursos.append("Todos")
+        return self.cursos
+    def comprobarCursoRepetido(self,cursos:list,checkCurso:str) -> bool:
+        comprobar = True
+        i = 0
+        while(i<cursos.__len__()):
+            if(cursos[i]==checkCurso):
+                comprobar=False
+                break
+            i+=1
+        return comprobar
+
+    def mostrarAlumnoPorCurso(self,curso:str) -> list:
+        """
+        Metodo que devuelve una lista de alumnos en funcion de su curso
+        """
+        listaAlumnos:list[Alumno] = ALUMNOS_OPERATOR.readFile()
+        alumnos:list[Alumno] = []
+        if(curso!="Todos"):
+            for alumno in listaAlumnos:
+                if(alumno.curso==curso):
+                    alumnos.append(alumno)
+        else:
+            alumnos = listaAlumnos
+        alumnosParse = []
+        for alumno in alumnos:
+            alumnosParse = alumno.nombre+", "+alumno.apellido
+        return alumnosParse
+
+    def onChangeCurso(self,widget):
+        self.selectionAlumnos.items = self.mostrarAlumnoPorCurso(curso=self.selectionCurso.value)
+
